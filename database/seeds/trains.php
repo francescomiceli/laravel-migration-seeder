d97c8c47273fd96bfcb50b11ba17741277b48b9e
francesco.miceli <francescomiceli@outlook.com>
2022-11-03 17:59:09 +0100

    last push

--- database/seeds/trains.php
+++ database/seeds/trains.php
@@ -14,7 +14,25 @@ class trains extends Seeder
     {
         for($i = 0; $i < 50; $i++){
             $newTrain = new train();
-            $newTrain-> Azienda = $faker
+            $newTrain-> Azienda = $faker->words(1);
+            $newTrain-> Stazione_di_partenza = $faker->country();
+            $newTrain-> Stazione_di_arrivo = $faker->country();
+            $newTrain-> Orario_di_partenza = $faker->time('H:i:s');
+            $newTrain-> Orario_di_arrivo = $faker->time('H:i:s');
+            $newTrain-> codice_treno = $faker->randomNumber(1, false);
+            $newTrain-> numero_carrozze = $faker->randomNumber(1, false);
+            $newTrain-> Data_partenza = $faker->date('Y_m_d');
+            $newTrain-> In_orario = $faker->boolean();
+            $newTrain->Cancellato = $faker->boolean();
+            $newTrain->save();
+
+
+
+
+
+
+
+
         }
     }
 }
